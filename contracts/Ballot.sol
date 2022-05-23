1a2f1ecc7ab2bd4a5b58b06bbcff068f9416922d
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-05-23 16:48:00 +0500

    Some of the tests in mocha

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -3,11 +3,10 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Ballot {
-
     struct Candidate {
         address candidateAddress;
         string name;
-        uint votes;
+        uint256 votes;
     }
 
     // struct Vote {
@@ -19,55 +18,55 @@ contract Ballot {
         address voterAddress;
     }
 
-    
     Candidate[] public candidates;
 
     string state;
 
+    constructor() {}
 
-    constructor() {
-        
-    }
-
-    function startVoting() public  {
-        
-    }
+    function startVoting() public {}
 
     function endVoting() public view {
         winnerName();
     }
 
-    function addCandidate(Candidate memory candidate_) public{
-         candidates.push(candidate_);
+    function addCandidate(Candidate memory candidate_)
+        public
+        returns (uint256 totalCount)
+    {
+        candidates.push(candidate_);
+        totalCount = candidates.length;
+        return totalCount;
     }
 
+    function countCandidates() public view returns (uint256 totalCount) {
+        totalCount = candidates.length;
+        return totalCount;
+    }
 
-    function transferEths(uint _value) public payable{
-        address payable addr = payable(candidates[winningCandidate()].candidateAddress);
+    function transferEths(uint256 _value) public payable {
+        address payable addr = payable(
+            candidates[winningCandidate()].candidateAddress
+        );
         addr.transfer(_value);
     }
 
-    function castVote(address candidateAddress) public{
-        for (uint p = 0; p < 2; p++) {
+    function castVote(address candidateAddress) public {
+        for (uint256 p = 0; p < 2; p++) {
             if (candidates[p].candidateAddress == candidateAddress) {
-                candidates[p].votes+=1;
+                candidates[p].votes += 1;
             }
         }
     }
 
-    function winnerName() public view
-            returns (string memory winnerName_)
-    {
+    function winnerName() public view returns (string memory winnerName_) {
         winnerName_ = candidates[winningCandidate()].name;
         return winnerName_;
     }
 
-
-     function winningCandidate() public view
-            returns (uint winningProposal_)
-    {
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < 2; p++) {
+    function winningCandidate() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < 2; p++) {
             if (candidates[p].votes > winningVoteCount) {
                 winningVoteCount = candidates[p].votes;
                 winningProposal_ = p;
@@ -75,5 +74,4 @@ contract Ballot {
             }
         }
     }
-
 }
